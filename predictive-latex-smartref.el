a661dcfb368d10f16a26891e56089ed51e5fa53e
Toby Cubitt <toby-predictive@dr-qubit.org>
2007-05-08 19:16:18 +0000

    Updated for new version of smartref package.

--- predictive-latex-smartref.el
+++ predictive-latex-smartref.el
@@ -3,10 +3,10 @@
 ;;;                                  package support
 
 
-;; Copyright (C) 2004-2006 Toby Cubitt
+;; Copyright (C) 2004-2007 Toby Cubitt
 
 ;; Author: Toby Cubitt <toby-predictive@dr-qubit.org>
-;; Version: 0.2.1
+;; Version: 0.3
 ;; Keywords: predictive, latex, package, smartref, sref
 ;; URL: http://www.dr-qubit.org/emacs.php
 
@@ -31,6 +31,9 @@
 
 ;;; Change Log:
 ;;
+;; Version 0.3
+;; * updated for new version of smartref package
+;;
 ;; Version 0.2.1
 ;; * updated `completion-override-syntax-alist' settings to reflect changes in
 ;;   predictive-latex.el
@@ -54,13 +57,19 @@
       predictive-latex-usepackage-functions)
 
 
+;; set up 'predictive-latex-smartref-label-word to be a `thing-at-point'
+;; symbol
+(put 'predictive-latex-smartref-label-word 'forward-op
+     'predictive-latex-smartref-label-forward-word)
+
+
 
 (defun predictive-latex-load-smartref ()
   ;; Load sref regexps
   (auto-overlay-load-compound-regexp
    `(start "\\\\sref{" (dict . predictive-latex-label-dict) (priority . 2)
 	   (completion-menu . predictive-latex-construct-browser-menu)
-	   (completion-word-thing . predictive-latex-label-word)
+	   (completion-word-thing . predictive-latex-smartref-label-word)
 	   (completion-dynamic-syntax-alist . ((?w . (add t word))
 					       (?_ . (add t word))
 					       (?  . (accept t none))
@@ -73,13 +82,14 @@
 	       (?_ . ((lambda ()
 			(predictive-latex-completion-add-to-regexp "\\W"))
 		      t word))
+	       (?, . (accept t none))
 	       (?} . (accept t none))))
 	   (face . (background-color . ,predictive-latex-debug-color)))
    'predictive 'brace t 'sref)
   (auto-overlay-load-compound-regexp
    `(start "\\\\Sref{" (dict . predictive-latex-label-dict) (priority . 2)
 	   (completion-menu . predictive-latex-construct-browser-menu)
-	   (completion-word-thing . predictive-latex-label-word)
+	   (completion-word-thing . predictive-latex-smartref-label-word)
 	   (completion-dynamic-syntax-alist . ((?w . (add t word))
 					       (?_ . (add t word))
 					       (?  . (accept t none))
@@ -92,6 +102,7 @@
 	       (?_ . ((lambda ()
 			(predictive-latex-completion-add-to-regexp "\\W"))
 		      t word))
+	       (?, . (accept t none))
 	       (?} . (accept t none))))
 	   (face . (background-color . ,predictive-latex-debug-color)))
    'predictive 'brace t 'Sref)
@@ -103,6 +114,35 @@
   ;; Unload sref regexps
   (auto-overlay-unload-regexp 'predictive 'brace 'sref)
   (auto-overlay-unload-regexp 'predictive 'brace 'Sref)
+)
+
+
+
+(defun predictive-latex-smartref-label-forward-word (&optional n)
+  (let (m)
+    ;; going backwards...
+    (if (and n (< n 0))
+	(unless (bobp)
+	  (setq m (- n))
+	  (when (= ?\\ (char-before))
+	    (while (= ?\\ (char-before)) (backward-char))
+	    (setq m (1- m)))
+	  (dotimes (i m)
+	    (backward-word 1)  ; argument not optional in Emacs 21
+	    (while (and (char-before)
+			(or (= (char-syntax (char-before)) ?w)
+			    (= (char-syntax (char-before)) ?_)
+			    (and (= (char-syntax (char-before)) ?.)
+				 (/= (char-before) ?,))))
+	      (backward-char))))
+      ;; going forwards...
+      (unless (eobp)
+	(setq m (if n n 1))
+	(dotimes (i m)
+	  (if (re-search-forward "\\(\\w\\|\\s_\\|\\s.\\)+" nil t)
+	      (when (= (char-before) ?,) (backward-char))
+	    (goto-char (point-max)))))
+      ))
 )
 
 ;;; predictive-latex-smartref ends here
