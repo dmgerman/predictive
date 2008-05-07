6c5c30c7147bd77f0035f3147561867bd0783b61
Toby Cubitt <toby-predictive@dr-qubit.org>
2008-05-07 10:09:17 +0000

    Included Eric Ludlum's changes.

--- completion-ui-semantic.el
+++ completion-ui-semantic.el
@@ -1,23 +1,36 @@
 
 (defun semantic-prefix-wrapper ()
-  "Returns prefix at point that Semantic would complete."
-  (let ((prefix (semantic-ctxt-current-symbol (point))))
-    (setq prefix (nth (1- (length prefix)) prefix))
-    (set-text-properties 0 (length prefix) nil prefix)
-    prefix)
+  "Return prefix at point that Semantic would complete."
+  (when (semantic-idle-summary-useful-context-p)
+    (let ((prefix (semantic-ctxt-current-symbol (point))))
+      (setq prefix (nth (1- (length prefix)) prefix))
+      (set-text-properties 0 (length prefix) nil prefix)
+      prefix))
 )
 
 
 (defun semantic-completion-wrapper (prefix maxnum)
-  "Returns list of Semantic completions for prefix at point."
-  (let* ((ctxt (semantic-analyze-current-context))
-	 (acomp (semantic-analyze-possible-completions ctxt)))
-    (when (> (length acomp) maxnum)
-      (setq acomp (subseq acomp 0 (1- maxnum))))
-    (mapcar (lambda (a)
-	      (substring (semantic-tag-name a) (length prefix)))
-	    acomp))
-)
+  "Return list of Semantic completions for PREFIX at point.
+Argument MAXNUM is the maximum number of ."
+  (when (semantic-idle-summary-useful-context-p)
+    (let* (
+	   ;; Don't go loading in oodles of header libraries in
+	   ;; for minor completions
+	   ;; @todo - Don't do this iff the user pressed a key
+	   ;;         indicating they don't mind the longer wait.
+	   (semanticdb-find-default-throttle
+	    (if (featurep 'semanticdb-find)
+		(remq 'unloaded semanticdb-find-default-throttle)
+	      nil))
+
+	   (ctxt (semantic-analyze-current-context))
+	   (acomp (semantic-analyze-possible-completions ctxt)))
+      (when (> (length acomp) maxnum)
+	(setq acomp (subseq acomp 0 (1- maxnum))))
+      (mapcar (lambda (a)
+		(substring (semantic-tag-name a) (length prefix)))
+	      acomp))
+    ))
 
 
 (defun completion-setup-semantic ()
