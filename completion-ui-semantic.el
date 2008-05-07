3225ee082fc4f879438009f2e44a5ef28d9bca9e
Toby Cubitt <toby-predictive@dr-qubit.org>
2008-05-07 10:11:17 +0000

    Made "." always invoke auto-completion (only appropriate for c-like languages, so needs improving).

--- completion-ui-semantic.el
+++ completion-ui-semantic.el
@@ -9,33 +9,36 @@
 )
 
 
+
 (defun semantic-completion-wrapper (prefix maxnum)
   "Return list of Semantic completions for PREFIX at point.
 Argument MAXNUM is the maximum number of ."
   (when (semantic-idle-summary-useful-context-p)
     (let* (
-	   ;; Don't go loading in oodles of header libraries in
-	   ;; for minor completions
-	   ;; @todo - Don't do this iff the user pressed a key
-	   ;;         indicating they don't mind the longer wait.
+	   ;; don't go loading in oodles of header libraries for minor
+	   ;; completions if using auto-completion-mode
+	   ;; FIXME: don't do this iff the user invoked completion manually
 	   (semanticdb-find-default-throttle
-	    (if (featurep 'semanticdb-find)
-		(remq 'unloaded semanticdb-find-default-throttle)
-	      nil))
-
+	    (when (and (featurep 'semanticdb-find)
+		       auto-completion-mode)
+	      (remq 'unloaded semanticdb-find-default-throttle)))
+	   
 	   (ctxt (semantic-analyze-current-context))
 	   (acomp (semantic-analyze-possible-completions ctxt)))
       (when (> (length acomp) maxnum)
 	(setq acomp (subseq acomp 0 (1- maxnum))))
       (mapcar (lambda (a)
 		(substring (semantic-tag-name a) (length prefix)))
-	      acomp))
-    ))
+	      acomp)))
+)
+
 
 
 (defun completion-setup-semantic ()
-  "Setup Semantic completion-UI support."
+  "Setup Semantic Completion-UI support."
   (interactive)
   (setq completion-function 'semantic-completion-wrapper)
   (setq completion-prefix-function 'semantic-prefix-wrapper)
+  (setq auto-completion-override-syntax-alist '((?. . (add word))))
+  (define-key completion-map "." 'completion-self-insert)
 )
