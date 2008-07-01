8ee55b28f24800e1ea16fe90c8f1704918622d4d
Toby Cubitt <toby-predictive@dr-qubit.org>
2008-07-01 12:40:59 +0000

    Examples for completion-function, from Emacs Wiki CompletionUI page.

---
+++ completion-ui-examples.el
@@ -0,0 +1,32 @@
+
+;;;=========================================================
+;;;                     dabbrevs
+
+(require 'dabbrev)
+
+(defun dabbrev--wrapper (prefix maxnum)
+  "Wrapper around `dabbrev--find-all-completions',
+to use as a `completion-function'."
+  (dabbrev--reset-global-variables)
+  (let ((completions (dabbrev--find-all-expansions prefix nil)))
+    (when maxnum
+      (setq completions
+	    (butlast completions (- (length completions) maxnum))))
+    completions))
+
+(setq completion-function 'dabbrev--wrapper)
+
+
+;;;=========================================================
+;;;                        etags
+
+(defun etags--wrapper (prefix maxnum)
+  "Wrapper around a call to `all-completions'
+using `tags-complete-tag', to use as a `completion-function'."
+  (let ((completions (all-completions prefix 'tags-complete-tag)))
+    (when maxnum
+      (setq completions
+	    (butlast completions (- (length completions) maxnum))))
+    completions))
+
+(setq completion-function 'etags--wrapper)
