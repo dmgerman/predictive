cc8ddb2ed2ddb9130a00c65651d76238d94d4a9f
Toby Cubitt <toby-predictive@dr-qubit.org>
2008-10-31 16:55:12 +0000

    predictive-convert-dump-format now also fixes null word weights

--- predictive-convert-dump-format.el
+++ predictive-convert-dump-format.el
@@ -4,17 +4,25 @@
 of the predictive package to version 21 (i.e. from version < 0.17.x
 and earlier of the \"predictive.el\" library to version 0.18)."
   (interactive)
+  ;; convert format line by line
   (goto-char (point-min))
-  (if (search-forward "(:prefixes (" nil t)
-      (error "File %s appears to have already been converted to\
- the new format" (file-name-nondirectory (buffer-file-name)))
-    (while (not (eobp))
-      (unless (eolp) (forward-sexp))
-      (unless (eolp) (forward-sexp))
-      (unless (or (eolp) (null (search-forward "(" (line-end-position) t)))
-	(backward-char)
-	(insert "(:prefixes ")
-	(goto-char (line-end-position))
-	(insert ")"))
-      (forward-line))
-    (save-buffer)))
+  (while (not (eobp))
+    (unless (eolp) (forward-sexp))
+    ;; convert null weight to 0
+    (cond
+     ((eolp) (insert " 0"))
+     ((looking-at "[[:space:]]+[[:digit:]]+\\([[:space:]]\\|$\\)")
+      (forward-sexp))
+     ((looking-at "[[:space:]]+\\(nil\\)\\([[:space:]]\\|$\\)")
+      (replace-match "0" nil nil nil 1)))
+    ;; convert prefix list format
+    (unless (or (eolp)
+		(looking-at "[[:space:]]+(:prefixes[[:space:]]")
+		(null (search-forward "(" (line-end-position) t)))
+      (backward-char)
+      (insert "(:prefixes ")
+      (goto-char (line-end-position))
+      (insert ")"))
+    (forward-line))
+  ;; save converted file
+  (save-buffer))
