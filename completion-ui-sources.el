d04102ceb2def5dd0f275284fa7bac042b7e43ec
Toby S. Cubitt <toby-predictive@dr-qubit.org>
2013-06-20 18:27:28 +0200

    Added RefTeX completion-UI source.

--- completion-ui-sources.el
+++ completion-ui-sources.el
@@ -33,30 +33,38 @@
 ;; suppress compiler warnings
 (eval-when-compile
   (defvar semanticdb-find-default-throttle nil)
-  (require 'ispell))
-(if (fboundp 'declare-function)
-    (progn
-      (declare-function dabbrev--reset-global-variables
-			"dabbrev.el" nil)
-      (declare-function dabbrev--find-all-expansions
-			"dabbrev.el" (arg1 arg2))
-      (declare-function tags-lazy-completion-table
-			"etags.el" nil)
-      (declare-function semantic-idle-summary-useful-context-p
-			"ext:semantic-idle.el" nil)
-      (declare-function semantic-ctxt-current-symbol
-			"ext:semantic-ctxt.el" (&optional arg1))
-      (declare-function semantic-analyze-current-context
-			"ext:semantic-analyze.el" nil)
-      (declare-function semantic-analyze-possible-completions
-			"ext:semantic-analyze-complete.el" (arg1)))
-  (defun dabbrev--reset-global-variables nil)
-  (defun dabbrev--find-all-expansions (arg1 arg2))
-  (defun tags-lazy-complete-table nil)
-  (defun semantic-idle-summary-useful-context-p nil)
-  (defun semantic-ctxt-current-symbol (&optional arg1))
-  (defun semantic-analyze-current-context nil)
-  (defun semantic-analyze-possible-completions (arg1)))
+  (require 'ispell)
+  (require 'dabbrev)
+  (require 'etags)
+  (require 'flyspell)
+  (require 'nxml nil t)
+  (require 'reftex nil t)
+  (require 'semantic-ia nil t)
+
+  (if (fboundp 'declare-function)
+      (progn
+	(declare-function dabbrev--reset-global-variables
+			  "dabbrev.el" nil)
+	(declare-function dabbrev--find-all-expansions
+			  "dabbrev.el" (arg1 arg2))
+	(declare-function tags-lazy-completion-table
+			  "etags.el" nil)
+	(declare-function semantic-idle-summary-useful-context-p
+			  "ext:semantic-idle.el" nil)
+	(declare-function semantic-ctxt-current-symbol
+			  "ext:semantic-ctxt.el" (&optional arg1))
+	(declare-function semantic-analyze-current-context
+			  "ext:semantic-analyze.el" nil)
+	(declare-function semantic-analyze-possible-completions
+			  "ext:semantic-analyze-complete.el" (arg1)))
+    (defun dabbrev--reset-global-variables nil)
+    (defun dabbrev--find-all-expansions (arg1 arg2))
+    (defun tags-lazy-complete-table nil)
+    (defun semantic-idle-summary-useful-context-p nil)
+    (defun semantic-ctxt-current-symbol (&optional arg1))
+    (defun semantic-analyze-current-context nil)
+    (defun semantic-analyze-possible-completions (arg1)))
+  )
 
 
 
@@ -118,10 +126,8 @@ available for combining."
 	(delete `(const nil) choices)
 	(nconc choices `((const ,name)))))))
 
-
 (add-hook 'completion-ui-register-source-functions
 	  'completion-ui-update-combine-sources-defcustom)
-
 
 
 (defun completion-ui-combining-complete (source-spec prefix &optional maxnum)
@@ -167,22 +173,12 @@ sexp
 	completions))))
 
 
-
 (completion-ui-register-source
  completion-ui-combining-complete
  :completion-args (2 3)
  :other-args (completion-ui-combine-sources-alist)
  :name Combine
  :no-combining t)
-
-
-;; (completion-ui-register-source
-;;  completion-ui-combining-complete
-;;  :completion-args (2)
-;;  :other-args (completion-ui-combine-sources-alist)
-;;  :name Combine-freq
-;;  :sort-by-frequency global
-;;  :no-combining t)
 
 
 ;;;=========================================================
@@ -196,15 +192,6 @@ sexp
  :name dabbrev)
 
 
-;; (completion-ui-register-source
-;;  (lambda (prefix)
-;;    (require 'dabbrev)
-;;    (dabbrev--reset-global-variables)
-;;    (dabbrev--find-all-expansions prefix case-fold-search))
-;;  :name dabbrev-freq
-;;  :sort-by-frequency global)
-
-
 ;;;=========================================================
 ;;;                        etags
 
@@ -213,14 +200,6 @@ sexp
    (require 'etags)
    (all-completions prefix (tags-lazy-completion-table)))
  :name etags)
-
-
-;; (completion-ui-register-source
-;;  (lambda (prefix)
-;;    (require 'etags)
-;;    (all-completions prefix (tags-lazy-completion-table)))
-;;  :name etags-freq
-;;  :sort-by-frequency global)
 
 
 ;;;=========================================================
@@ -234,29 +213,19 @@ sexp
  :word-thing symbol)
 
 
-;; (completion-ui-register-source
-;;  all-completions
-;;  :completion-args 1
-;;  :other-args (obarray)
-;;  :name elisp-freq
-;;  :word-thing symbol
-;;  :sort-by-frequency global)
-
-
 ;;;=========================================================
 ;;;                        file names
 
-(eval-and-compile
-  (defun completion--filename-wrapper (prefix)
-    ;; Return filename completions of prefix
-    (let ((dir (file-name-directory prefix))
-	  completions)
-      (mapc (lambda (file)
-	      (unless (or (string= file "../") (string= file "./"))
-		(push (concat dir file) completions)))
-	    (file-name-all-completions
-	     (file-name-nondirectory prefix) dir))
-      (nreverse completions))))
+(defun completion--filename-wrapper (prefix)
+  ;; Return filename completions of prefix
+  (let ((dir (file-name-directory prefix))
+	completions)
+    (mapc (lambda (file)
+	    (unless (or (string= file "../") (string= file "./"))
+	      (push (concat dir file) completions)))
+	  (file-name-all-completions
+	   (file-name-nondirectory prefix) dir))
+    (nreverse completions)))
 
 
 (completion-ui-register-source
@@ -264,42 +233,35 @@ sexp
  :name files)
 
 
-;; (completion-ui-register-source
-;;  completion--filename-wrapper
-;;  :name files-freq
-;;  :sort-by-frequency global)
-
-
 ;;;=========================================================
 ;;;                         ispell
 
-(eval-and-compile
-  (defun completion--ispell-wrapper (word)
-    (require 'flyspell)
-    (let (suggestions ispell-filter)
-      ;; Now check spelling of word.
-      (ispell-send-string "%\n") ; put in verbose mode
-      (ispell-send-string (concat "^" word "\n")) ; lookup the word
-      ;; Wait until ispell has processed word.
-      (while (progn
-	       (accept-process-output ispell-process)
-	       (not (string= "" (car ispell-filter)))))
-      ;; Remove leading empty element
-      (setq ispell-filter (cdr ispell-filter))
-      ;; ispell process should return something after word is sent.
-      ;; Tag word as valid (i.e., skip) otherwise
-      (or ispell-filter
-	  (setq ispell-filter '(*)))
-      (when (consp ispell-filter)
-	(setq suggestions (ispell-parse-output (car ispell-filter))))
-      (cond
-       ((or (eq suggestions t) (stringp suggestions))
-	(message "Ispell: %s is correct" word)
-	nil)
-       ((null suggestions)
-	(error "Ispell: error in Ispell process")
-	nil)
-       (t (car (cdr (cdr suggestions))))))))
+(defun completion--ispell-wrapper (word)
+  (require 'flyspell)
+  (let (suggestions ispell-filter)
+    ;; Now check spelling of word.
+    (ispell-send-string "%\n") ; put in verbose mode
+    (ispell-send-string (concat "^" word "\n")) ; lookup the word
+    ;; Wait until ispell has processed word.
+    (while (progn
+	     (accept-process-output ispell-process)
+	     (not (string= "" (car ispell-filter)))))
+    ;; Remove leading empty element
+    (setq ispell-filter (cdr ispell-filter))
+    ;; ispell process should return something after word is sent.
+    ;; Tag word as valid (i.e., skip) otherwise
+    (or ispell-filter
+	(setq ispell-filter '(*)))
+    (when (consp ispell-filter)
+      (setq suggestions (ispell-parse-output (car ispell-filter))))
+    (cond
+     ((or (eq suggestions t) (stringp suggestions))
+      (message "Ispell: %s is correct" word)
+      nil)
+     ((null suggestions)
+      (error "Ispell: error in Ispell process")
+      nil)
+     (t (car (cdr (cdr suggestions)))))))
 
 
 (completion-ui-register-source
@@ -308,38 +270,76 @@ sexp
  :name ispell)
 
 
-;; (completion-ui-register-source
-;;  completion--ispell-wrapper
-;;  :non-prefix-completion t
-;;  :name ispell-freq
-;;  :sort-by-frequency global)
-
-
 
 ;;;=========================================================
 ;;;                        NXML
 
-(when (require 'nxml nil t)
+(when (locate-library "nxml")
   (completion-ui-register-source
-   rng-complete-qname-function
+   (lambda (prefix)
+     (require nxml)
+     (rng-complete-qname-function prefix))
    :completion-args 1
    :other-args (t t)
    :name nxml)
+)
 
-  ;; (completion-ui-register-source
-  ;;  rng-complete-qname-function
-  ;;  :completion-args 1
-  ;;  :other-args (t t)
-  ;;  :name nxml-freq
-  ;;  :sort-by-frequency global)
-  )
 
+;;;=========================================================
+;;;                        RefTeX
+
+(when (locate-library "reftex")
+  (put 'latex-label 'forward-op 'latex-label-forward-word)
+
+  ;; copied from predictive-latex.el
+  (defun latex-label-forward-word (&optional n)
+    ;; going backwards...
+    (if (and n (< n 0))
+	(unless (bobp)
+	  (setq n (- n))
+	  (when (eq (char-before) ?\\)
+	    (while (eq (char-before) ?\\) (backward-char))
+	    (setq n (1- n)))
+	  (dotimes (i n)
+	    (when (and (char-before) (= (char-syntax (char-before)) ?w))
+	      (backward-word 1))  ; argument not optional in Emacs 21
+	    (while (and (char-before)
+			(or (= (char-syntax (char-before)) ?w)
+			    (= (char-syntax (char-before)) ?_)
+			    (and (= (char-syntax (char-before)) ?.)
+				 (/= (char-before) ?{))))
+	      (backward-char))))
+      ;; going forwards...
+      (unless (eobp)
+	(setq n (if n n 1))
+	(dotimes (i n)
+	  (when (and (char-after) (= (char-syntax (char-after)) ?w))
+	    (forward-word 1))  ; argument not optional in Emacs 21
+	  (while (and (char-after)
+		      (or (= (char-syntax (char-after)) ?w)
+			  (= (char-syntax (char-after)) ?_)
+			  (and (= (char-syntax (char-after)) ?.)
+			       (/= (char-after) ?}))))
+	    (forward-char))))))
+
+
+  (completion-ui-register-source
+   (lambda (prefix)
+     (all-completions
+      prefix
+      (delq nil (mapcar (lambda (c) (when (stringp (car c)) (car c)))
+			reftex-docstruct-symbol-1))))
+   :name reftex
+   :word-thing latex-label
+   :no-auto-completion t
+   :no-predictive t)
+)
 
 
 ;;;=========================================================
 ;;;                        Semantic
 
-(when (require 'semantic nil t)
+(when (locate-library "semantic")
 
   (defun completion--semantic-prefix-wrapper ()
     ;; Return prefix at point that Semantic would complete.
@@ -351,26 +351,25 @@ sexp
 	prefix)))
 
 
-  (eval-and-compile
-    (defun completion--semantic-wrapper (prefix &optional maxnum)
-      ;; Return list of Semantic completions for PREFIX at point. Optional
-      ;; argument MAXNUM is the maximum number of completions to return.
-      (require 'semantic-ia)
-      (when (semantic-idle-summary-useful-context-p)
-	(let* (
-	       ;; don't go loading in oodles of header libraries for minor
-	       ;; completions if using auto-completion-mode
-	       ;; FIXME: don't do this iff the user invoked completion manually
-	       (semanticdb-find-default-throttle
-		(when (and (featurep 'semanticdb-find)
-			   auto-completion-mode)
-		  (remq 'unloaded semanticdb-find-default-throttle)))
+  (defun completion--semantic-wrapper (prefix &optional maxnum)
+    ;; Return list of Semantic completions for PREFIX at point. Optional
+    ;; argument MAXNUM is the maximum number of completions to return.
+    (require 'semantic-ia)
+    (when (semantic-idle-summary-useful-context-p)
+      (let* (
+	     ;; don't go loading in oodles of header libraries for minor
+	     ;; completions if using auto-completion-mode
+	     ;; FIXME: don't do this iff the user invoked completion manually
+	     (semanticdb-find-default-throttle
+	      (when (and (featurep 'semanticdb-find)
+			 auto-completion-mode)
+		(remq 'unloaded semanticdb-find-default-throttle)))
 
-	       (ctxt (semantic-analyze-current-context))
-	       (acomp (semantic-analyze-possible-completions ctxt)))
-	  (when (and maxnum (> (length acomp) maxnum))
-	    (setq acomp (butlast acomp (- (length acomp) maxnum))))
-	  (mapcar 'semantic-tag-name acomp)))))
+	     (ctxt (semantic-analyze-current-context))
+	     (acomp (semantic-analyze-possible-completions ctxt)))
+	(when (and maxnum (> (length acomp) maxnum))
+	  (setq acomp (butlast acomp (- (length acomp) maxnum))))
+	(mapcar 'semantic-tag-name acomp))))
 
 
   (defun completion--semantic-enable-auto-completion nil
@@ -379,12 +378,10 @@ sexp
       (set (make-local-variable 'auto-completion-override-syntax-alist)
 	   '((?. . (add word))))))
 
-
   (defun completion--semantic-disable-auto-completion nil
     ;; unset buffer-local variables when disabling Semantic auto-completion
     (when (eq auto-completion-default-source 'semantic)
       (kill-local-variable 'auto-completion-override-syntax-alist)))
-
 
   (add-hook 'auto-completion-mode-enable-hook
 	    'completion--semantic-enable-auto-completion)
@@ -396,14 +393,9 @@ sexp
   (completion-ui-register-source
    completion--semantic-wrapper
    :prefix-function completion--semantic-prefix-wrapper
+   :completion-args 2
    :name semantic)
-
-  ;; (completion-ui-register-source
-  ;;  completion--semantic-wrapper
-  ;;  :prefix-function completion--semantic-prefix-wrapper
-  ;;  :name semantic-freq
-  ;;  :sort-by-frequency global)
-  )
+)
 
 
 
